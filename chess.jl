c3678e3bdbcc53b64887f1c98a4f36ace2dd3f7e
loganjmaurer <loganmaurer@protonmail.com>
2024-07-10 11:24:29 -0500

    Add files via upload

--- chess.jl
+++ chess.jl
@@ -1,51 +1,60 @@
-using CSV, DataFrames, ChesspicApi, CategoricalArrays, MLDataUtils, Flux, Statistics
+using BayesFlux
+using Flux
+using CSV
+using DataFrames
+using Random
 
-# Load the data
-games = CSV.read("games.csv", DataFrame)
+# Read the data
+data = CSV.read("chess_games.csv", DataFrame)
+white_elo = data.white_elo
+black_elo = data.black_elo
+moves = data.moves
+winner = data.Winner
 
-# Extract the moves for each game
-function get_moves(moves_str)
-    return split(moves_str, " ")
-end
-moves_df = DataFrame(
-    game_id = games.game_id,
-    move = mapreduce(get_moves, vcat, games.moves)
+# Encode the moves
+move_vocab = unique(vcat(moves...))
+move_to_idx = Dict(move => i for (i, move) in enumerate(move_vocab))
+encoded_moves = [[move_to_idx[m] for m in game_moves] for game_moves in moves]
+
+# Set the target and input
+y = [winner == "White" for winner in winner]
+X = encoded_moves
+
+# Randomize the data
+perm = randperm(length(X))
+X = X[perm]
+y = y[perm]
+
+# Split the data into train and test sets
+train_size = Int(floor(0.8 * length(X)))
+train_X, train_y = X[1:train_size], y[1:train_size]
+test_X, test_y = X[train_size+1:end], y[train_size+1:end]
+
+# Set the priors based on Elo ratings
+elo_diff = [white_elo[i] - black_elo[i] for i in 1:length(white_elo)]
+prior = NetworkPrior(
+    Flux.Normal[Flux.Normal(elo_diff[i] / 400, 1) for i in 1:length(move_vocab)],
+    Flux.Gamma[Flux.Gamma(1, 1) for _ in 1:64]
 )
 
-# Create a one-hot encoding for the moves
-unique_moves = unique(moves_df.move)
-moves_onehot = to_categorical(moves_df.move, unique_moves)
-moves_onehot_matrix = Matrix{Float64}(undef, length(games.game_id), length(unique_moves))
-for (i, game_id) in enumerate(games.game_id)
-    game_moves = moves_onehot[moves_df.game_id .== game_id, :]
-    moves_onehot_matrix[i, :] = sum(game_moves, dims=1) ./ size(game_moves, 1)
-end
-
-# Prepare the input and target variables
-X = hcat(Matrix{Float64}(games[:, [:rating_diff, :turns, :white_rating, :black_rating]]), moves_onehot_matrix)
-y = games.winner
-train_X, test_X, train_y, test_y = splitdata(X, y, Train=0.8, Test=0.2)
-
-# Normalize the input features
-train_X_norm = (train_X .- mean(train_X, dims=1)) ./ std(train_X, dims=1)
-test_X_norm = (test_X .- mean(train_X, dims=1)) ./ std(train_X, dims=1)
-
-# Define and train the neural network
-model = Chain(
-    Dense(size(X, 2), 32, relu),
-    Dense(32, 16, relu),
-    Dense(16, 3)
+# Define the model
+model = BNN(
+    train_X, train_y,
+    BNNLikelihood(Flux.Sigmoid()),
+    prior,
+    BNNInitialiser(Flux.kaiming_normal)
 )
-loss(x, y) = Flux.crossentropy(model(x), Flux.onehot(y, 1:3))
-opt = Flux.Momentum(0.01)
-epochs = 50
-for i = 1:epochs
-    gs = Flux.gradient(() -> loss(train_X_norm, train_y), Flux.params(model))
-    Flux.update!(opt, Flux.params(model), gs)
+
+# Train the model
+train_steps = 1000
+for i in 1:train_steps
+    Flux.train!(model, [(train_X, train_y)], Flux.ADAM())
+    if i % 100 == 0
+        println("Iteration $i, Training Loss: $(loss(model, train_X, train_y))")
+    end
 end
 
-# Evaluate the model
-train_accuracy = mean(Flux.onehot(model(train_X_norm), 1:3) .== Flux.onehot(train_y, 1:3))
-test_accuracy = mean(Flux.onehot(model(test_X_norm), 1:3) .== Flux.onehot(test_y, 1:3))
-println("Train Accuracy: $train_accuracy")
+# Evaluate the model on the test set
+test_probs = Flux.predict_proba(model, test_X)
+test_accuracy = mean(Flux.onehot(argmax(test_probs, dims=2), 1:2) .== Flux.onehot(test_y, 1:2))
 println("Test Accuracy: $test_accuracy")
